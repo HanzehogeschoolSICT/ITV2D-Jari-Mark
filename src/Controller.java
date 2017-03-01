a5da1e1f6dc190652272d6dd44409076763855aa
LittleBox <ja.kuipers@st.hanze.nl>
2017-03-01 10:50:41 +0100

    Added TODO to UpdateGUI()

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,4 @@
-package sample;
-
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,10 +12,13 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.ResourceBundle;
 
+@SuppressWarnings("unchecked")
 public class Controller implements Initializable {
+    ArrayList<Integer> list;
     String sortingtype = "BubbleSort";
     Random random = new Random();
     int n = 6;
+    XYChart.Series series;
 
     @FXML
     private BarChart<?, ?> randomchart;
@@ -25,12 +27,12 @@ public class Controller implements Initializable {
     private ComboBox<?> methodbox;
 
 
-    @SuppressWarnings("unchecked")
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         randomchart.setLegendVisible(false);
+        randomchart.setAnimated(false);
 
-        ArrayList<Integer> list = new ArrayList<>();
+        list = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             list.add(i);
         }
@@ -39,7 +41,7 @@ public class Controller implements Initializable {
             list.add(temp);
         }
 
-        XYChart.Series series = new XYChart.Series();
+        series = new XYChart.Series();
 
         for (int i = 0; i < list.size(); i++) {
             series.getData().add(new XYChart.Data(list.get(i).toString(), list.get(i)));
@@ -49,19 +51,51 @@ public class Controller implements Initializable {
 
     @FXML
     void changeString(ActionEvent event) {
+        // Called when the combobox value is changed
         sortingtype = (String) methodbox.getValue();
     }
 
     @FXML
     void doStep(MouseEvent event) {
+        // Called when the step button is clicked
         switch (sortingtype) {
             case "BubbleSort":
                 SortOneBubble();
                 break;
+
+            case "InsertionSort":
+                SortOneInsertion();
+                break;
+        }
+        UpdateGUI();
+    }
+
+    private void UpdateGUI() {
+        // TODO: Fix GUI update
+        series = new XYChart.Series();
+
+        for (Integer aList : list) {
+            series.getData().add(new XYChart.Data(aList.toString(), aList));
         }
     }
 
-    private void SortOneBubble() {
+    private void SortOneInsertion() {
+        // TODO: Make Injection sort
+    }
 
+    private void SortOneBubble() {
+        // TODO: Make bubblesort
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                int temp = list.get(i);
+                int temp2 = list.get(i + 1);
+                list.set(i + 1, temp);
+                list.set(i, temp2);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
     }
 }
