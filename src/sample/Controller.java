751aeced43396bbd2d60942c9d0b3b3543b55bd3
LittleBox <ja.kuipers@st.hanze.nl>
2017-03-04 17:00:36 +0100

    Bubblesort works but needs a lot of improvement.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 
@@ -14,14 +15,16 @@ import java.util.Random;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    int n = 5;
-    GraphicsContext gc;
-    ArrayList<Integer> list;
-    int maxval = 0;
+    private int n = 10;
+    private GraphicsContext gc;
+    private ArrayList<Integer> list;
+    private int maxval = 0;
+    private Phase phase = Phase.SELECTING;
+    private int switchpos1;
+    private int switchpos2;
 
     @FXML
     private Canvas barcanvas;
-
     @FXML
     private Button stepbutton;
 
@@ -31,18 +34,65 @@ public class Controller implements Initializable {
     }
 
     private void BubbleStep() {
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i) > list.get(i + 1)) {
-                int temp = list.get(i);
-                int temp2 = list.get(i + 1);
-                list.set(i + 1, temp);
-                list.set(i, temp2);
-            }
+        switch (phase) {
+            case SELECTING:
+                boolean result = doSelect();
+                if (result){
+                    phase = Phase.SWITCHING;
+                } else {
+                    sortedMessage();
+                }
+                break;
+
+            case SWITCHING:
+                doSwitch();
+                phase = Phase.SELECTING;
+                break;
         }
+    }
+
+    // source: http://code.makery.ch/blog/javafx-dialogs-official/
+    private void sortedMessage() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("info");
+        alert.setHeaderText(null);
+        alert.setContentText("The list is sorted!");
+        alert.showAndWait();
+    }
+
+    private void doSwitch() {
+        int temp1 = list.get(switchpos1);
+        int temp2 = list.get(switchpos2);
+        list.set(switchpos1, temp2);
+        list.set(switchpos2, temp1);
         clear();
         for (int i = 0; i < list.size(); i++) {
+            if (i == switchpos1 || i == switchpos2) {
+                drawBar(i, list.get(i), Color.GREEN);
+            } else {
+                drawBar(i, list.get(i), Color.RED);
+            }
+        }
+    }
+
+    private boolean doSelect() {
+        clear();
+        boolean found = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (i + 1 < list.size() && list.get(i) > list.get(i + 1) && !found) {
+                drawBar(i, list.get(i), Color.GREEN);
+                drawBar(i + 1, list.get(i + 1), Color.GREEN);
+                i++;
+                found = true;
+                switchpos1 = i - 1;
+                switchpos2 = i;
+                continue;
+            } else {
+                drawBar(i, list.get(i), Color.RED);
+            }
             drawBar(i, list.get(i), Color.RED);
         }
+        return found;
     }
 
     @Override
@@ -58,7 +108,6 @@ public class Controller implements Initializable {
                 maxval = temp;
             list.add(temp);
         }
-        maxval++;
         for (int i = 0; i < list.size(); i++) {
             drawBar(i, list.get(i), Color.RED);
         }
@@ -74,7 +123,11 @@ public class Controller implements Initializable {
         gc.strokeRect((xaxis * xoffset - 1), barcanvas.getHeight() - yaxis * yoffset, xoffset + 1, yaxis * yoffset);
     }
 
-    private void clear(){
-        gc.clearRect(0,0, barcanvas.getWidth(), barcanvas.getHeight());
+    private void clear() {
+        gc.clearRect(0, 0, barcanvas.getWidth(), barcanvas.getHeight());
+    }
+
+    private enum Phase {
+        SELECTING, SWITCHING
     }
 }
